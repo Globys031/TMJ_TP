5aa14f9bb25aa8e4e6a3d3f1d8e66e9d8913b7fe
Matas <m.tunkevicius@gmail.com>
2021-03-06 20:27:01 +0200

    wireframe of UI

--- CustomList/Form1.Designer.cs
+++ CustomList/Form1.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.b_custom = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pn_Nav = new System.Windows.Forms.FlowLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.b_TvSeries = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.b_Dashboard = new System.Windows.Forms.Button();
             this.pfpic = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.b_TvSeries = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pfpic)).BeginInit();
@@ -44,7 +45,8 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.pn_Nav);
+            this.panel1.Controls.Add(this.b_custom);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.b_TvSeries);
             this.panel1.Controls.Add(this.button1);
@@ -56,6 +58,23 @@
             this.panel1.Size = new System.Drawing.Size(951, 97);
             this.panel1.TabIndex = 0;
             // 
+            // b_custom
+            // 
+            this.b_custom.Dock = System.Windows.Forms.DockStyle.Left;
+            this.b_custom.FlatAppearance.BorderSize = 0;
+            this.b_custom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b_custom.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_custom.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.b_custom.Location = new System.Drawing.Point(809, 0);
+            this.b_custom.Name = "b_custom";
+            this.b_custom.Size = new System.Drawing.Size(178, 97);
+            this.b_custom.TabIndex = 1;
+            this.b_custom.Text = "will scroll later";
+            this.b_custom.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.b_custom.UseVisualStyleBackColor = true;
+            this.b_custom.Click += new System.EventHandler(this.b_custom_Click);
+            this.b_custom.Leave += new System.EventHandler(this.b_custom_Leave);
+            // 
             // panel2
             // 
             this.panel2.Controls.Add(this.pfpic);
@@ -64,6 +83,69 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(97, 97);
             this.panel2.TabIndex = 0;
+            // 
+            // pn_Nav
+            // 
+            this.pn_Nav.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.pn_Nav.Location = new System.Drawing.Point(103, 3);
+            this.pn_Nav.Name = "pn_Nav";
+            this.pn_Nav.Size = new System.Drawing.Size(178, 3);
+            this.pn_Nav.TabIndex = 1;
+            this.pn_Nav.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.button3.Image = global::CustomList.Properties.Resources.anime;
+            this.button3.Location = new System.Drawing.Point(631, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 97);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Anime";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Leave += new System.EventHandler(this.button3_Leave);
+            // 
+            // b_TvSeries
+            // 
+            this.b_TvSeries.Dock = System.Windows.Forms.DockStyle.Left;
+            this.b_TvSeries.FlatAppearance.BorderSize = 0;
+            this.b_TvSeries.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b_TvSeries.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_TvSeries.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.b_TvSeries.Image = global::CustomList.Properties.Resources.tv;
+            this.b_TvSeries.Location = new System.Drawing.Point(453, 0);
+            this.b_TvSeries.Name = "b_TvSeries";
+            this.b_TvSeries.Size = new System.Drawing.Size(178, 97);
+            this.b_TvSeries.TabIndex = 1;
+            this.b_TvSeries.Text = "TV Series";
+            this.b_TvSeries.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.b_TvSeries.UseVisualStyleBackColor = true;
+            this.b_TvSeries.Click += new System.EventHandler(this.b_TvSeries_Click);
+            this.b_TvSeries.Leave += new System.EventHandler(this.b_TvSeries_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.button1.Image = global::CustomList.Properties.Resources.clapperboard;
+            this.button1.Location = new System.Drawing.Point(275, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 97);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Movies";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Leave += new System.EventHandler(this.button1_Leave);
             // 
             // b_Dashboard
             // 
@@ -80,6 +162,8 @@
             this.b_Dashboard.Text = "Dashboard";
             this.b_Dashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.b_Dashboard.UseVisualStyleBackColor = true;
+            this.b_Dashboard.Click += new System.EventHandler(this.b_Dashboard_Click);
+            this.b_Dashboard.Leave += new System.EventHandler(this.b_Dashboard_Leave);
             // 
             // pfpic
             // 
@@ -90,68 +174,6 @@
             this.pfpic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pfpic.TabIndex = 0;
             this.pfpic.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.button1.Location = new System.Drawing.Point(275, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 97);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Movies";
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // b_TvSeries
-            // 
-            this.b_TvSeries.Dock = System.Windows.Forms.DockStyle.Left;
-            this.b_TvSeries.FlatAppearance.BorderSize = 0;
-            this.b_TvSeries.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.b_TvSeries.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.b_TvSeries.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.b_TvSeries.Location = new System.Drawing.Point(453, 0);
-            this.b_TvSeries.Name = "b_TvSeries";
-            this.b_TvSeries.Size = new System.Drawing.Size(178, 97);
-            this.b_TvSeries.TabIndex = 1;
-            this.b_TvSeries.Text = "TV Series";
-            this.b_TvSeries.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.b_TvSeries.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.button3.Image = global::CustomList.Properties.Resources.anime;
-            this.button3.Location = new System.Drawing.Point(631, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(178, 97);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "Dashboard";
-            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.button4.Image = global::CustomList.Properties.Resources.home;
-            this.button4.Location = new System.Drawing.Point(809, 0);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(178, 97);
-            this.button4.TabIndex = 1;
-            this.button4.Text = "Dashboard";
-            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -164,7 +186,8 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Custom List";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pfpic)).EndInit();
@@ -178,10 +201,11 @@
         private System.Windows.Forms.Button b_Dashboard;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pfpic;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button b_custom;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button b_TvSeries;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel pn_Nav;
     }
 }
 
--- CustomList/Properties/Resources.Designer.cs
+++ CustomList/Properties/Resources.Designer.cs
@@ -73,9 +73,29 @@ namespace CustomList.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap clapperboard {
+            get {
+                object obj = ResourceManager.GetObject("clapperboard", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap home {
             get {
                 object obj = ResourceManager.GetObject("home", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap tv {
+            get {
+                object obj = ResourceManager.GetObject("tv", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
