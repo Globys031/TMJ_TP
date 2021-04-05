4cc077f95df39ad0456a3650146458ffa5277e66
SuveizdisT <79005863+SuveizdisT@users.noreply.github.com>
2021-04-05 15:35:25 +0300

    Basic SQL methods  (#10)
    * Add DB
    
    * testing
    
    * mock-up of entries
    
    * added window handle
    
    * Added basic SQL methods
    
    * Mums reikia butinai gitignore, kad netrackintu failu, kurie nereikia
    
    * Fixed merge conflict
    
    * Continuing to take care of merge conflicts
    
    * Modified .gitignore
    
    * Still trying to take care of merge conflicts
    
    * Still trying to merge
    
    * Still trying to resolve merge conflict
    
    * merged main
    
    Co-authored-by: Matas <m.tunkevicius@gmail.com>
    Co-authored-by: Matas Tunkevičius <78436416+matasx8@users.noreply.github.com>
    Co-authored-by: Globys031 <52034687+Globys031@users.noreply.github.com>

---
+++ CustomList/DatabaseClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CustomList
+{
+    public static class DatabaseClass
+    {
+        public static string ConnectionVal()
+        {
+            return ConfigurationManager.ConnectionStrings["ListDatabase"].ConnectionString;
+        }
+        public static string connectionLine = ConnectionVal();
+        static SqlConnection connection;
+
+        #region buttons
+
+        #endregion
+        public static void SetData(string name, string image, double score, string des, string date)
+        {
+            string query = "INSERT INTO Entry VALUES (@Entry, @image, @score, @des, @date)";
+
+            using (connection = new SqlConnection(connectionLine))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue("@Entry", name);
+                command.Parameters.AddWithValue("@image", image);
+                command.Parameters.AddWithValue("@score", score);
+                command.Parameters.AddWithValue("@des", des);
+                command.Parameters.AddWithValue("@date", date);
+                command.ExecuteScalar();
+            }
+
+            //ShowCategory();
+            //ShowEntries();
+        }
+        public static List<Entry> GetDataByCategory(string CategoryName)
+        {
+            int CatId;
+            string query = "SELECT a.Id FROM Category a WHERE a.name =@catname";
+            using (connection = new SqlConnection(connectionLine))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue(@"catname", CategoryName);
+                CatId = Convert.ToInt32(command.ExecuteScalar());
+            }
+            return ShowCategoryEntries(CatId);
+
+        }
+        private static List<Entry> ShowCategoryEntries(int Id)
+        {
+            List<Entry> data = new List<Entry>();
+            string query = "SELECT a.name FROM Entry a INNER JOIN CategoryEntry b ON a.Id = b.EntryId " +
+                "WHERE b.CategoryId = @CategoryId";
+            using (connection = new SqlConnection(connectionLine))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@CategoryId", Id);
+                DataTable catEntTable = new DataTable();
+                adapter.Fill(catEntTable);
+
+                /*listCatEnt.DisplayMember = "name";
+                listCatEnt.ValueMember = "Id";
+                listCatEnt.DataSource = catEntTable;*/
+                foreach (DataRow dr in catEntTable.Rows)
+                {
+                    Entry entry = new Entry(dr.ToString());
+                    data.Add(entry);
+                }
+            }
+            return data;
+        }
+    }
+}
