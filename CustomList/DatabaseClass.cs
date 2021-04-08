ec7ce2d2f444c56cd6d8ec42202cf1dbb65d6e3b
SuveizdisT <79005863+SuveizdisT@users.noreply.github.com>
2021-04-08 20:44:13 +0300

    Add fixed insert method (#12)

--- CustomList/DatabaseClass.cs
+++ CustomList/DatabaseClass.cs
@@ -22,24 +22,65 @@ namespace CustomList
         #region buttons
 
         #endregion
-        public static void SetData(string name, string image, double score, string des, string date)
+        public static void SetData(string Category, string name, string image, double score, string des, string date)
         {
-            string query = "INSERT INTO Entry VALUES (@Entry, @image, @score, @des, @date)";
-
+            string query = "INSERT INTO Entry (name, image, score, description, date_of_entry)" +
+                " VALUES (@name, NULL, @score, @description, @date_of_entry); SELECT SCOPE_IDENTITY()";
+            int newID;
             using (connection = new SqlConnection(connectionLine))
             using (SqlCommand command = new SqlCommand(query, connection))
             {
                 connection.Open();
-                command.Parameters.AddWithValue("@Entry", name);
-                command.Parameters.AddWithValue("@image", image);
+                command.Parameters.AddWithValue("@name", name);
+                //command.Parameters.AddWithValue("@image", 
                 command.Parameters.AddWithValue("@score", score);
-                command.Parameters.AddWithValue("@des", des);
-                command.Parameters.AddWithValue("@date", date);
+                command.Parameters.AddWithValue("@description", des);
+                command.Parameters.AddWithValue("@date_of_entry", date);
+                newID = Convert.ToInt32(command.ExecuteScalar());
+            }
+            ConnectEntryAndCategory(Category, newID);
+        }
+        public static void ConnectEntryAndCategory(string Category, int entryId)
+        {
+            int CatId = 0;
+            string query = "SELECT a.Id FROM Category a WHERE a.name =@catname";
+            using(connection = new SqlConnection(connectionLine))
+            using(SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue(@"catname", Category);
+                if(command != null)
+                    CatId = Convert.ToInt32(command.ExecuteScalar());
+            }
+            if(CatId == 0)
+            {
+                CatId = SetCategory(Category);
+                InsertCatEnt(CatId, entryId);
+            }
+            else InsertCatEnt(CatId, entryId);
+        }
+        public static int SetCategory(string Category)
+        {
+            string query = "INSERT INTO Category (name) VALUES (@name); SELECT SCOPE_IDENTITY()";
+            using(connection = new SqlConnection(connectionLine))
+            using(SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue(@"name", Category);
+                return Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+        private static void InsertCatEnt(int CatId, int entryId)
+        {
+            string query = "INSERT INTO CategoryEntry (CategoryId, EntryId) VALUES (@CategoryId, @EntryId)";
+            using (connection = new SqlConnection(connectionLine))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                command.Parameters.AddWithValue(@"CategoryId", CatId);
+                command.Parameters.AddWithValue(@"EntryId", entryId);
                 command.ExecuteScalar();
             }
-
-            //ShowCategory();
-            //ShowEntries();
         }
         public static List<Entry> GetDataByCategory(string CategoryName)
         {
@@ -53,7 +94,9 @@ namespace CustomList
                 command.Parameters.AddWithValue(@"catname", CategoryName);
                 CatId = Convert.ToInt32(command.ExecuteScalar());
             }
-            return ShowCategoryEntries(CatId);
+            if (CatId != 0)
+                return ShowCategoryEntries(CatId);
+            else return null;
 
         }
         private static List<Entry> ShowCategoryEntries(int Id)
@@ -69,14 +112,8 @@ namespace CustomList
                 command.Parameters.AddWithValue("@CategoryId", Id);
                 DataTable catEntTable = new DataTable();
                 adapter.Fill(catEntTable);
-
-                /*listCatEnt.DisplayMember = "name";
-                listCatEnt.ValueMember = "Id";
-                listCatEnt.DataSource = catEntTable;*/
                 foreach (DataRow dr in catEntTable.Rows)
                 {
-
-                    //Entry entry = new Entry(dr.ItemArray[0].ToString());
                     string name = dr[1].ToString();
                     string image = dr[2].ToString();
                     double score = double.Parse(dr[3].ToString());
