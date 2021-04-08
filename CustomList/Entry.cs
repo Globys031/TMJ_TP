12b2d24fb9375d9af81e78d51d945a13b18b11ff
Titas Suveizdis <titassuveizdis@gmail.com>
2021-04-08 11:07:15 +0300

    fix SQL method

--- CustomList/Entry.cs
+++ CustomList/Entry.cs
@@ -11,17 +11,21 @@ namespace CustomList
     {
         private string name; // name of the entry (e.g. "The mummy 1999")
         // placeholder for image
-        private int score; // score for entry (e.g. 2, would be 2/10)
+        private double score; // score for entry (e.g. 2, would be 2/10)
         private string description; // description of the entry
+        private string image;
+        private string date;
         // later on the user will be able to add aditional columns as he desires
         // (e.g. progress, location, ect...)
         private string[] additionalColumns;
 
-        public Entry(string Name, int Score, string Description)
+        public Entry(string Name, string Image, double Score, string Description, string Date)
         {
             this.name = Name;
+            this.image = Image;
             this.score = Score;
             this.description = Description;
+            this.date = Date;
         }
         public Entry(string Name)
         {
