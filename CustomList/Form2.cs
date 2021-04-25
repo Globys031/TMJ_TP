529dc8e09c7e8737a22ba31b53fd408cd7018c58
Matas Tunkevičius <m.tunkevicius@gmail.com>
2021-04-25 13:48:03 +0300

    Added dropdown for rating, fixed small bugs, added validation

--- CustomList/Form2.cs
+++ CustomList/Form2.cs
@@ -15,13 +15,14 @@ namespace CustomList
         private List<EntryComponent> entryComponents;
         private string category;
         private ChildForm parent;
-        private string posterPath;
         public Form2(List<EntryComponent> entryComponents, string category, ChildForm parent)
         {
             InitializeComponent();
             this.entryComponents = entryComponents;
            this.category = category;
             this.parent = parent;
+
+            drpdRating.SelectedIndex = 0;
         }
 
         private void btnExitAddDialogue_Click(object sender, EventArgs e)
@@ -33,9 +34,19 @@ namespace CustomList
         private void btnSave_Click(object sender, EventArgs e)
         {
             string name = txtEntryName.Text;
-            string rating = txtRating.Text;
+            string rating = drpdRating.SelectedItem.ToString();
             string description = txtEntryDescription.Text;
             string dateTime = dateRelease.Value.Date.ToShortDateString();
+
+            var errors = ValidateFields(name, description);
+            string ErrorMsg;
+            if (errors != "")
+            {
+                ErrorMsg = "Can't save, errors: " + errors;
+                lblAnyText.Text = ErrorMsg;
+                return;
+            }
+
 
             var ent = new Entry(name, openFileDialog1.FileName, Int32.Parse(rating), description, dateTime);
             entryComponents.Add(new EntryComponent(entryComponents.Count + 1, ent, category));
@@ -55,7 +66,34 @@ namespace CustomList
         private void btnSelectPoster_Click(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();
-           // posterPath = openFileDialog1.
+        }
+        private string ValidateFields(string name, string description)
+        {
+            string error = "";
+            if(name.Length == 0)
+            {
+                error += " name of your entry must not be empty.";
+            }
+            else if (name.Length > 50)
+            {
+                error += " name of your entry must not longer than 50 characters.";
+            }
+            if(description.Length == 0)
+            {
+                error += " description must not be empty.";
+            }
+            if(openFileDialog1.FileName != "")
+            {
+                try
+                {
+                    Image.FromFile(openFileDialog1.FileName);
+                }
+                catch (Exception)
+                {
+                    error += " the file you selected can't be used as a poster";
+                }
+            }
+            return error;
         }
     }
 }
